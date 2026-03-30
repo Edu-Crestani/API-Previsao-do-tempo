d47291eb3e52e70d1538d989229900ded7058cc7
Gabriel Gemelli <gemelligabriel380@gmail.com>
2026-03-30 18:13:08 -0300

    Alerts

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-let display = document.getElementById("display");
+document.querySelector('#search').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const cityName = document.querySelector('#city_name').value;
+
+    if (!cityName) {
+        return showAlert('Você precisa digitar uma cidade...');
+    }
+
+    const apiKey = '7762cffeeabd70453beaa75f31a66bce';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric&lang=pt_br`;
+
+    const results = await fetch(apiUrl);
+    const json = await results.json();
+
+    if (json.cod === 200) {
+        showAlert('Cidade encontrada!');
+      } else {
+        showAlert('Cidade não encontrada...');
+      }
+    }
+});
+
+function showAlert(msg) {
+    document.querySelector('#alert').innerHTML = msg;
+}
